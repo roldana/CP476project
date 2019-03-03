838846617fd466c65e5c373fef383119e6136b79
Lucas Boulanger <boul9440@mylaurier.ca>
2019-03-03 13:34:29 -0500

    added info.php

---
+++ php/info.php
@@ -0,0 +1,13 @@
+<?php
+    include("../include/header.php");
+?>
+
+    <div class="content-container">
+        <div class="content-wrap">
+            <p> This is where information about the site and its functionality will eventually go (including the pdf for submission). </p>
+        </div>
+    </div>
+
+<?php
+    include("../include/footer.php");
+?>
