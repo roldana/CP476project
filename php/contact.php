62d02dc32a7b375ac6158e1f3f2f1db291d4f04c
Luc <boul9440@mylaurier.ca>
2019-03-22 16:58:38 -0400

    contact us messages now save to folder /inquires in text files

--- php/contact.php
+++ php/contact.php
@@ -1,12 +1,22 @@
 <?php
     include("../include/header.php");
+    
+    if (isset($_REQUEST) and isset($_REQUEST['body'])) {
+          $fileLocation = getenv("DOCUMENT_ROOT") . "/CP476project/inquires/".time().".txt";
+          $file = fopen($fileLocation,"w");
+          $content = "{\"data\": {\"email\": \"".$_REQUEST['email']."\",";
+          $content = $content."\n\"subject\": \"".$_REQUEST['subject']."\",";
+          $content = $content."\n\"body\": \"".$_REQUEST['body']."\"}}";
+          fwrite($file,$content);
+          fclose($file);
+    }
 ?>
 
     <div class="content-container">
         <div class="content-wrap">        
             
             <span class="form-title">
-                Send Us An Email
+                Send Us A Message
             </span>
             <p> This is where you will be able to send us a message, and where we will be able to ignore them. </p>
             <form id="contact-form" method="POST" action="contact.php">
@@ -17,7 +27,7 @@
                     <input class="input-box" type="text" name="subject" placeholder="Subject">
                 </div>
                 <div class="wrap-input">
-                    <textarea class="input-area" type="text" name="body" placeholder="Message Body" rows="16"></textarea>
+                    <textarea class="input-area" type="text" name="body" placeholder="Message Body" rows="10"></textarea>
                 </div>
                     <button type="submit" class="btn btn-outline-success float-r">Send Message</button>
                 </div>
