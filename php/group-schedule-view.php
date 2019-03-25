6e47379de514b84921d0a0c5f4db788224e3343b
Lucas Boulanger <boul9440@mylaurier.ca>
2019-03-25 11:34:33 -0400

    new chat template in group-schedule-view

--- php/group-schedule-view.php
+++ php/group-schedule-view.php
@@ -563,30 +563,59 @@
         </button>
     </div>
 
-</div>
-
-<div class="comment-box content-wrap">
-    <b>Comments</b>
-</div>
-
-<div class="comment-box content-wrap">
-    <b>Username2:</b> Let's rule out thursday and friday.
-</div>
-
-<div class="comment-box content-wrap">
-    <b>Username3:</b> The due date has been extended!
-</div>
-
-<div class="comment-box content-wrap">
-    <b>Username1:</b><br>
-    <input type="text" class="comment-input" id="userComment" size="70">
-
-    <div class="submit-button-box">
-        <button class="btn btn-outline-success">
-            Submit Comment
-        </button>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card card-inverse col-md-6 ml-auto">
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="row list-group-item">
+                            <div class="col-md-10">
+                                <p>
+                                    <strong>Someone Else</strong>
+                                    <small>March 29th, 2019 @ 5:20PM</small> 
+                                </p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                            dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="row list-group-item">
+                            <div class="col-md-10 ml-auto">
+                                <p class="text-right">
+                                    <small>March 29th, 2019 @ 5:22PM</small>
+                                    <strong>(You)</strong> 
+                                </p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                            dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="row list-group-item">
+                            <div class="col-md-10">
+                                <p>
+                                    <strong>Someone Else</strong>
+                                    <small>March 29th, 2019 @ 5:24PM</small>
+                                </p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                            dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-footer">
+                    <div class="input-group">
+                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-success" id="btn-chat">Send</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
 </div>
 <br>
 
