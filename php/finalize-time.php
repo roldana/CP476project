25fd5d146f5037e8a873f709bb2019e69eaefca8
Michael Solski <michaelavsolski@rogers.com>
2019-03-31 22:25:27 -0400

    Google calendar makes event based on finalization
    Final times not added yet however

--- php/finalize-time.php
+++ php/finalize-time.php
@@ -73,15 +73,150 @@
                           ?>
                     </div>
                     <div class="card-footer">
-                        <button id="submit-times" type="button" class="btn btn-success btn-lg float-r group-view-btn">Submit Times</a>
+                        <button id="submit-times" type="button" class="btn btn-success btn-lg float-r group-view-btn">Submit Times</button>
                     </div>
                     <?php } ?>
                 </div>
             </div>
         </div>
+        <br>
+        <iframe id="calFrame" style="display: none;" src="https://calendar.google.com/calendar/embed?src=cp476project%40gmail.com&ctz=America%2FToronto" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
 </div>
 </div>
+
+<!-- <p id="signin">Sign into your Google account to access Google Calendar</p>
+<button class="btn btn-success float-l" id="authorize_button" style="display: none;">Google Sign In</button> -->
+
+<?php
+echo '<p id ="groupName" style="display: none;">' . $group['GroupName'] . "</p>";
+echo '<p id ="description" style="display: none;">' . $group['Description'] . "</p>";
+
+echo '<p id ="finalStart" style="display: none;">' . $group['FinalStart'] . "</p>";
+echo '<p id ="finalEnd" style="display: none;">' . $group['FinalEnd'] . "</p>";
+
+// Get the user's emails
+$userEmails = retreiveEmailsFromGroup($db, $_GET['GroupID']);
+$i = 0;
+foreach($userEmails as $x){
+    echo '<p id ="userEmail' . $i . '"' . 'style="display: none;">' . $x[0] . "</p>";
+    $i++;
+}
+echo '<p id ="i" style="display: none;">' . $i . "</p>";
+?>
+
+<script src="https://apis.google.com/js/api.js"></script>
+<script type="text/javascript">
+    var CLIENT_ID = '467375927024-3lcl0fhv0u4thk2r3dpqno9n710tar13.apps.googleusercontent.com';
+    var API_KEY = 'AIzaSyAQ8mAvGVnkbJOulP-ZcG7ZX0N93wIt3P0';
+    var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+    var SCOPES = "https://www.googleapis.com/auth/calendar.events";
+
+    var authorizeButton = document.getElementById('submit-times');
+    var calendarFrame = document.getElementById('calFrame');
+
+    var groupName = document.getElementById('groupName').innerHTML;
+    var description = document.getElementById('description').innerHTML;
+
+    var finalStart = document.getElementById('finalStart').innerHTML;
+    var finalEnd = document.getElementById('finalEnd').innerHTML;
+
+    var emails = [];
+    for(var x=0; x<document.getElementById('i').innerHTML; x++){
+        var email = document.getElementById("userEmail" + x).innerHTML;
+
+        var emailDict = {
+            email: email
+        }
+        emails.push(emailDict);
+    }
+
+    console.log(emails);
+
+    function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+    }
+    function initClient() {
+        gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES
+        }).then(function () {
+            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+            authorizeButton.onclick = handleAuthClick;
+        }, function(error) {
+            appendPre(JSON.stringify(error, null, 2));
+        });
+    }
+
+    function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+            //signinMessage.style.display = 'none';
+            authorizeButton.style.display = 'none';
+            createEvent();
+            calendarFrame.style.display = 'block';
+        } else {
+            authorizeButton.style.display = 'block';
+            calendarFrame.style.display = 'none';
+        }
+    }
+
+    function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+    }
+
+    function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+    }
+
+    function appendPre(message) {
+        var pre = document.getElementById('content');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+    }
+
+    function createEvent() {
+
+        var event = {
+            'summary': groupName + ' Meeting',
+            'description': description,
+            'start': {
+                'dateTime': '2019-03-31T09:00:00-07:00',
+                'timeZone': 'EST'
+            },
+            'end': {
+                'dateTime': '2019-03-31T17:00:00-07:00',
+                'timeZone': 'EST'
+            },
+            'attendees': emails,
+            'reminders': {
+                'useDefault': false,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10}
+                ]
+            }
+        };
+
+        var request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+        });
+
+        request.execute(function(event) {
+
+        });
+
+    }
+
+</script>
+
+<script async defer src="https://apis.google.com/js/api.js"
+        onload="this.onload=function(){};handleClientLoad()"
+        onreadystatechange="if (this.readyState === 'complete') this.onload()">
+</script>
 
 
 <?php include("../include/footer.php"); ?>
